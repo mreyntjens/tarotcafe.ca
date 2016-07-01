299d9c397e441d7e7ed32863e16428147c778679
remylab <remy.elazare@gmail.com>
2016-06-30 21:17:32 -0400

    back to gmail...

--- scripts/msgpost.php
+++ scripts/msgpost.php
@@ -1,15 +1,15 @@
 <?php
 if( isset($_POST['msg']) ) {
-	$to = "mreyntjens@gmail.com";
+	$to = "tarotcafemontreal@gmail.com";
 	
-	$subject = "Tarotcafe.ca :"  ; 
+	$subject = "tarotcafe.ca :"  ; 
 	if( isset($_POST['subject']) ) { 
         $subject .= " " . $_POST['subject'];  
     } 
 	
 	$message = 'Nom : ' . $_POST['name'] ;
 	$message .= ' / Courriel ou Tél. : ' . $_POST['coord'] . ' / Message : ' .  $_POST['msg'];
-	$from = "marienoelle@tarotcafe.ca";
+	$from = "no-reply@tarotcafe.ca";
 	$headers = "From:" . $from;
 	
 	mail($to,$subject,$message,$headers);
